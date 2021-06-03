1dbfbe9df62b4efc4a526f92090bc10fbde75603
SenecaAnderson <sanderson@mitre.org>
2021-06-03 08:07:53 -0400

    Submit button on Extract.js page triggers extraction process; extraction runs with test data and results in an output folder and result files

--- electron/preload.js
+++ electron/preload.js
@@ -2,3 +2,19 @@ const { ipcRenderer, contextBridge } = require('electron');
 
 // Update context bridge with any other API functions you need for renderer/main process communication
 contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer);
+
+contextBridge.exposeInMainWorld('api', {
+    hello: () => {
+        // do things!
+        // a function here will be used to call the function in electron.js, which will call the extraction framework
+        console.log('Hello from preload.js!');
+        return true;
+    },
+    extract: (fromDate, toDate, configFilepath, runLogFilepath, debug, allEntries) => {
+        //  call function in preload.js
+        ipcRenderer.invoke('run-extraction', fromDate, toDate, configFilepath, runLogFilepath, debug, allEntries).then((result) => {
+            // do something with result
+            console.log("extract invoke complete");
+        })
+    }
+});
--- react-app/src/components/Extract.js
+++ react-app/src/components/Extract.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import path from 'path';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Button, Col, Form, Row } from 'react-bootstrap';
 
@@ -8,14 +9,20 @@ class Extract extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      configPath: '',
-      logPath: '',
-      includeDebug: false,
-      filterStart: false,
-      filterEnd: false,
-    };
-  }
+        const defaultPathToConfig = path.join('test', 'config', 'csv.config.json');
+        const defaultPathToRunLogs = path.join('logs', 'run-logs.json');
+        // FIXME - get rid of default config option
+        this.state = {
+            configPath: defaultPathToConfig,
+            logPath: defaultPathToRunLogs,
+            includeDebug: false,
+            filterStart: false,
+            filterEnd: false,
+            fromDate: "",
+            toDate: "",
+            submitted: false
+        };
+    }
 
   onConfigFileChange = (e) => this.setState({ configPath: e.target.value });
 
@@ -29,68 +36,87 @@ class Extract extends React.Component {
 
   onSubmit = () => {
     // placeholder to avoid errors -- the filled-in version of this function in extractor-api uses "this"
-    this.setState({
-      configPath: this.state.configPath,
-    });
+    console.log(e);
+        console.log(this.state.configPath);
+
+        // allEntries parameter is true if it's not filtered by date, false if it is
+        const filter = !(this.state.filterStart || this.state.filterEnd);
+        window.api.extract(this.state.fromDate, 
+            this.state.toDate, 
+            this.state.configPath, 
+            this.state.runLogPath, 
+            this.state.includeDebug, 
+            filter);
+        
+        window.api.hello();
+        this.setState({
+            submitted: !this.state.submitted
+        });
   };
 
   render() {
     return (
       <div>
         <h1 className="page-title">Extract New</h1>
-        <Form>
-          <Row>
-            <Col>
-              <Form.Group controlId="formConfigPath" className="mb-3">
-                <Form.Label className="form-label">Configuration File</Form.Label>
-                <Form.Control onChange={this.onConfigFileChange} type="text" value={this.state.configPath} />
-              </Form.Group>
-              <Form.Group controlId="formLogPath">
-                <Form.Label className="form-label">Previous Log File</Form.Label>
-                <Form.Control onChange={this.onLogFileChange} type="text" value={this.state.logPath} />
-              </Form.Group>
-              <Form.Group controlId="formIncludeDebug">
-                <Form.Check
-                  checked={this.state.includeDebug}
-                  onChange={this.onIncludeDebugChange}
-                  type="checkbox"
-                  label="Log output debugging information"
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId="formIncludeStartDate">
-                <Form.Check
-                  checked={this.state.filterStart}
-                  onChange={this.onFilterStartChange}
-                  type="checkbox"
-                  label="Filter by start date"
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group controlId="formIncludeEndDate">
-                <Form.Check
-                  checked={this.state.filterEnd}
-                  onChange={this.onFilterEndChange}
-                  type="checkbox"
-                  label="Filter by end date"
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-        </Form>
-        <div className="nav-button-container">
-          <LinkContainer to="/">
-            <Button variant="danger">Cancel</Button>
-          </LinkContainer>
-          <Button type="submit" variant="info" onClick={this.onSubmit}>
-            Submit
-          </Button>
+        {!this.state.submitted && <div>
+            <Form>
+            <Row>
+                <Col>
+                <Form.Group controlId="formConfigPath" className="mb-3">
+                    <Form.Label className="form-label">Configuration File</Form.Label>
+                    <Form.Control onChange={this.onConfigFileChange} type="text" value={this.state.configPath} />
+                </Form.Group>
+                <Form.Group controlId="formLogPath">
+                    <Form.Label className="form-label">Previous Log File</Form.Label>
+                    <Form.Control onChange={this.onLogFileChange} type="text" value={this.state.logPath} />
+                </Form.Group>
+                <Form.Group controlId="formIncludeDebug">
+                    <Form.Check
+                        checked={this.state.includeDebug}
+                        onChange={this.onIncludeDebugChange}
+                        type="checkbox"
+                        label="Log output debugging information"
+                    />
+                </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <Form.Group controlId="formIncludeStartDate">
+                    <Form.Check
+                    checked={this.state.filterStart}
+                    onChange={this.onFilterStartChange}
+                    type="checkbox"
+                    label="Filter by start date"
+                    />
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group controlId="formIncludeEndDate">
+                    <Form.Check
+                    checked={this.state.filterEnd}
+                    onChange={this.onFilterEndChange}
+                    type="checkbox"
+                    label="Filter by end date"
+                    />
+                </Form.Group>
+                </Col>
+            </Row>
+            </Form>
+            <div className="nav-button-container">
+                <LinkContainer to="/">
+                    <Button variant="danger">Cancel</Button>
+                </LinkContainer>
+                <Button type="submit" variant="info" onClick={this.onSubmit}>
+                    Submit
+                </Button>
+            </div>
+          </div> }
+          {this.state.submitted && <div>
+                <p>The form as been submitted. Running extraction...</p>
+                <Button className="nav-button" type="submit" variant="nav" id="nav-button" onClick={this.onSubmit}>Reset</Button>
+            </div>}
         </div>
-      </div>
     );
   }
 }
