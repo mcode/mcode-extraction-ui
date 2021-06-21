b3c8f3e09a46020d1e1d7c054a72d13ae99474f9
SenecaAnderson <sanderson@mitre.org>
2021-06-21 10:19:41 -0400

    Made log message list a separate component; renamed associated scss file

---
+++ react-app/src/components/LogList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+
+import '../stylesheets/LogList.scss';
+
+function LogList(props) {
+  const list = props.loggedMessages.map((message, i) => (
+    <ListGroup.Item className="log-message" key={i}>
+      {message}
+    </ListGroup.Item>
+  ));
+  return <ListGroup>{list}</ListGroup>;
+}
+
+export default LogList;
--- react-app/src/components/PatientData.js
+++ react-app/src/components/PatientData.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
 import ReactJson from 'react-json-view';
-
-import '../stylesheets/LogFile.scss';
+import LogList from './LogList';
 
 function PatientData(props) {
-  const list = props.loggedMessages.map((message, i) => (
-    <ListGroup.Item className="log-message" key={i}>
-      {message}
-    </ListGroup.Item>
-  ));
   return (
     <div>
       {props.id === null && (
         <div className="page-text">
-          <p>Select a patient to view their information.</p>
+          <p>Select a patient to view their information or click on "Log File" to view the logger messages.</p>
         </div>
       )}
       {props.id !== null && props.id === -1 && (
         <div>
           <h3 className="page-subtitle">Log File</h3>
-          <ListGroup>{list}</ListGroup>
+          <LogList loggedMessages={props.loggedMessages} />
         </div>
       )}
       {props.id !== null && props.id >= 0 && (
