aaf60eb7659836a2e011d4e7ebed2f5dd9d1adf2
SenecaAnderson <sanderson@mitre.org>
2021-06-29 16:56:29 -0400

    Add resource counts to accordion bodies on sidebar of output display; add fhirpath and lodash dependencies

--- react-app/src/components/Result.js
+++ react-app/src/components/Result.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import { Accordion } from 'react-bootstrap';
 
+const _ = require('lodash');
+const fhirpath = require('fhirpath');
+
 function Result(props) {
+  function getResourceList() {
+    const resourceList = [];
+    // iterate through resources array, checking the resourceType of each entry
+    // if (new type),
+
+    const allResourceTypesPath = 'Bundle.descendants().resource.resourceType';
+    const uniqueResourceTypes = _.uniq(fhirpath.evaluate(props.bundle, allResourceTypesPath));
+
+    let totalResources = 0;
+    // FIXME - sort uniqueResourceTypes alphabetically
+
+    const countThisResource = (resourceType) =>
+      `Bundle.descendants().where(resource.resourceType = '${resourceType}').count()`;
+    uniqueResourceTypes.forEach((resourceType) => {
+      const count = fhirpath.evaluate(props.bundle, countThisResource(resourceType));
+      resourceList.push({ resourceType, count });
+      totalResources += parseInt(count, 10);
+    });
+
+    let sortedList = [{ resourceType: 'Total Resources', count: totalResources }];
+    sortedList = sortedList.concat(_.sortBy(resourceList, ['resourceType', 'count']));
+
+    return sortedList.map((item, i) => (
+      <p key={i}>
+        {item.resourceType}: {item.count}
+      </p>
+    ));
+  }
+
   return (
     <Accordion.Item eventKey={props.id}>
       <Accordion.Header
@@ -10,8 +42,9 @@ function Result(props) {
           props.setShowLogs(false);
         }}
       >
-        Patient {props.id}
+        Patient {props.id + 1}
       </Accordion.Header>
+      <Accordion.Body>{getResourceList()}</Accordion.Body>
     </Accordion.Item>
   );
 }
