4cae218daf6df139714955c9380312af663d8cb6
SenecaAnderson <sanderson@mitre.org>
2021-07-08 16:30:49 -0400

    Refactored fhirpath evaluations

--- react-app/src/components/Result.js
+++ react-app/src/components/Result.js
@@ -38,33 +38,30 @@ function Result(props) {
 
   function getLabel() {
     // attempt to get MRN and name with fhirpath
-    const mrn = fhirpath.evaluate(props.bundle, "Bundle.descendants().resource.where(resourceType='Patient').id")[0];
-    const name = fhirpath.evaluate(
-      props.bundle,
-      "Bundle.descendants().resource.where(resourceType='Patient').name.text",
-    )[0];
+    const patient = fhirpath.evaluate(props.bundle, "Bundle.descendants().resource.where(resourceType='Patient')")[0];
+    if (patient) {
+      const mrn = patient.id;
+      const name = patient.name[0].text;
 
-    let isMasked = fhirpath.evaluate(
-      props.bundle,
-      "Bundle.descendants().resource.where(resourceType='Patient').identifier.extension.valueCode",
-    )[0];
+      let isMasked = fhirpath.evaluate(patient, 'Patient.identifier.extension.valueCode')[0];
 
-    if (isMasked === 'masked') {
-      isMasked = true;
-    } else {
-      isMasked = false;
-    }
+      if (isMasked === 'masked') {
+        isMasked = true;
+      } else {
+        isMasked = false;
+      }
 
-    // if both MRN and name -- return string w / both
-    if (typeof mrn === 'string' && mrn.length > 0 && typeof name === 'string' && name.length > 0 && !isMasked) {
-      const label = mrn.concat(': ').concat(name);
-      return <p className="accordion-result-label">{label}</p>;
-    }
-    if (typeof name === 'string' && name.length > 0) {
-      return <p className="accordion-result-label">{name}</p>;
-    }
-    if (typeof mrn === 'string' && mrn.length > 0) {
-      return <p className="accordion-result-label">{mrn}</p>;
+      // if both MRN and name -- return string w / both
+      if (typeof mrn === 'string' && mrn.length > 0 && typeof name === 'string' && name.length > 0 && !isMasked) {
+        const label = mrn.concat(': ').concat(name);
+        return <p className="accordion-result-label">{label}</p>;
+      }
+      if (typeof name === 'string' && name.length > 0) {
+        return <p className="accordion-result-label">{name}</p>;
+      }
+      if (typeof mrn === 'string' && mrn.length > 0) {
+        return <p className="accordion-result-label">{mrn}</p>;
+      }
     }
 
     let label = 'Patient';
