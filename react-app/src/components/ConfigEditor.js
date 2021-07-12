e37c6e2673e3d250d4e69c2fccef9c2ddfd5497b
SenecaAnderson <sanderson@mitre.org>
2021-07-12 10:29:45 -0400

    Add 'Create New' toggle button and ConfigForm component; modify CSS styles to accomodate new pages

--- react-app/src/components/ConfigEditor.js
+++ react-app/src/components/ConfigEditor.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+import ConfigForm from './ConfigForm';
 
 function ConfigEditor() {
-  return <p>This is the configuration editor page!</p>;
+  const [showForm, setShowForm] = useState(false);
+
+  function toggleForm() {
+    setShowForm(!showForm);
+  }
+  return (
+    <div>
+      <h1 className="page-title">Configuration Editor</h1>
+      {!showForm && (
+        <div className="button-container">
+          <Button className="vertical-menu-button" variant="outline-secondary" onClick={toggleForm}>
+            Create New
+          </Button>
+        </div>
+      )}
+      {showForm && <ConfigForm />}
+    </div>
+  );
 }
 
 export default ConfigEditor;
