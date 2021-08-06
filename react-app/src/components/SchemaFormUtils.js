47ea63c42cb42a9455bbbd78ad2e69e316a80d4f
SenecaAnderson <sanderson@mitre.org>
2021-08-06 08:36:27 -0400

    format code with prettier

--- react-app/src/components/SchemaFormUtils.js
+++ react-app/src/components/SchemaFormUtils.js
@@ -3,76 +3,72 @@ function getConfigSchema() {
 }
 
 const uiSchema = {
-  'patientIdCsvPath': {
-    "classNames": "page-text"
+  patientIdCsvPath: {
+    classNames: 'page-text',
   },
-  'commonExtractorArgs': {
-    "classNames": "page-text",
-    'baseFhirUrl': {
-      "classNames": "page-text"
+  commonExtractorArgs: {
+    classNames: 'page-text',
+    baseFhirUrl: {
+      classNames: 'page-text',
     },
-    'requestHeaders': {
-      "classNames": "page-text"
-    },
-  },
-  'notificationInfo': {
-    "classNames": "page-text",
-    'host': {
-      "classNames": "page-text"
-    },
-    'port': {
-      "classNames": "page-text"
-    },
-    'from': {
-      "classNames": "page-text"
-    },
-    'to': {
-      "classNames": "page-text"
-    },
-    'tlsRejectUnauthorized': {
-      "classNames": "page-text"
+    requestHeaders: {
+      classNames: 'page-text',
     },
   },
-  'extractors': {
-    'extractor': {
-      "classNames": "page-text",
-      'label': {
-        "classNames": "page-text"
+  notificationInfo: {
+    classNames: 'page-text',
+    host: {
+      classNames: 'page-text',
+    },
+    port: {
+      classNames: 'page-text',
+    },
+    from: {
+      classNames: 'page-text',
+    },
+    to: {
+      classNames: 'page-text',
+    },
+    tlsRejectUnauthorized: {
+      classNames: 'page-text',
+    },
+  },
+  extractors: {
+    extractor: {
+      classNames: 'page-text',
+      label: {
+        classNames: 'page-text',
       },
-      'type': {
-        "classNames": "page-text"
+      type: {
+        classNames: 'page-text',
       },
-      'constructorArgs': {
-        "classNames": "page-text",
-        'filePath': {
-          "classNames": "page-text"
+      constructorArgs: {
+        classNames: 'page-text',
+        filePath: {
+          classNames: 'page-text',
         },
-        "url": {
-          "classNames": "page-text"
+        url: {
+          classNames: 'page-text',
         },
-        "clinicalSiteID": {
-          "classNames": "page-text"
+        clinicalSiteID: {
+          classNames: 'page-text',
         },
-        "clinicalSiteSystem": {
-          "classNames": "page-text"
+        clinicalSiteSystem: {
+          classNames: 'page-text',
         },
-        "type": {
-          "classNames": "page-text"
+        type: {
+          classNames: 'page-text',
         },
-        "mask": {
-          "classNames": "page-text"
+        mask: {
+          classNames: 'page-text',
         },
       },
     },
   },
 };
 
-const widgets = {
+const widgets = {};
 
-}
-
-const fields = {
-
-}
+const fields = {};
 
 module.exports = { getConfigSchema, uiSchema, widgets, fields };
