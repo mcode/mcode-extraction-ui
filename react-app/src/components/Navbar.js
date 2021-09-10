34e6c079770c6df80e3130e571ca9f0263ae2b42
Julia Afeltra <jafeltra@mitre.org>
2021-09-10 10:48:00 -0400

    Add eventKey and exact and use NavLink to properly support nav bar and collapsing

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Container, Nav, Navbar as NavBar } from 'react-bootstrap';
 
 function Navbar() {
   return (
     <NavBar collapseOnSelect fixed="top" variant="dark" expand="lg" className="navbar">
       <Container fluid>
-        <NavBar.Brand as={Link} to="/" className="navbar-brand">
+        <NavBar.Brand as={NavLink} to="/" className="navbar-brand">
           mCODE Extractor
         </NavBar.Brand>
         <NavBar.Toggle aria-controls="basic-navbar-nav" />
         <NavBar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#" as={Link} to="/">
+            <Nav.Link eventKey="home" exact as={NavLink} to="/">
               Home
             </Nav.Link>
-            <Nav.Link href="#extract" as={Link} to="/extract">
+            <Nav.Link eventKey="extract" exact as={NavLink} to="/extract">
               Extract New
             </Nav.Link>
-            <Nav.Link href="#config-editor" as={Link} to="/config-editor">
+            <Nav.Link eventKey="config-editor" exact as={NavLink} to="/config-editor">
               Configuration Editor
             </Nav.Link>
           </Nav>
