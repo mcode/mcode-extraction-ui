dab6d8d5a26c8584c2b129158026b584f5aaacaf
Julia Afeltra <jafeltra@mitre.org>
2021-12-01 14:42:05 -0500

    Switch to HashRouter, use replace on NavLinks, and use Link instead of history.push

--- react-app/src/components/CommonComponents/LinkButton.js
+++ react-app/src/components/CommonComponents/LinkButton.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 function LinkButton(props) {
-  const history = useHistory();
-  const onClick = () => {
-    history.push(props.path);
-  };
   return (
-    <Button className={props.className} variant={props.variant} onClick={onClick}>
-      {props.text}
-    </Button>
+    <Link to={props.path}>
+      <Button className={props.className} variant={props.variant} size={props.size}>
+        {props.text}
+      </Button>
+    </Link>
   );
 }
 
